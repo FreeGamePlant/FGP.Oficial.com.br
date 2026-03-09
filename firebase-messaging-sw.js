9c831a996f354b537c38aff70fb947d63390041e
FGP <elaifigueroa@gmail.com>
2026-03-09 08:23:23 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -14,6 +14,6 @@ const messaging = firebase.messaging();
 messaging.onBackgroundMessage(function(payload) {
   self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
-    icon: "/icon.png"
+    icon: "/FGP.Oficial.com.br/src/assets/images/lOGOFGP.png"
   });
 });
